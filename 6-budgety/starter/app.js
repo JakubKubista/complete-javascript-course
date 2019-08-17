c016afefbdd281451d935409b085fdbeabfc31ca
Jakub Kubista <jakub.kubista@noriginmedia.com>
2019-08-18 00:47:27 +0200

    add feature for adding new items

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -1,7 +1,51 @@
 let budgetController = (() => {
 
-    return {
+    let Income = function (id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
 
+    let Expense = function (id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+
+    let data = {
+        items: {
+            inc: [],
+            exp: []
+        },
+        counts: {
+            inc: 0,
+            exp: 0
+        }
+    };
+
+    return {
+        addItem: (type, description, value) => {
+            let newItem, ID;
+
+            if (data.items[type].length > 0) {
+                ID = data.items[type][data.items[type].length - 1].id + 1;
+            } else {
+                ID = 0;
+            }
+
+            if (type === 'inc') {
+                newItem = new Income(ID, description, value);
+            } else if (type === 'exp') {
+                newItem = new Expense(ID, description, value);
+            }
+
+            data.items[type].push(newItem);
+            return newItem;
+        },
+
+        logging: () => {
+            console.log(data);
+        }
     }
 
 })();
@@ -10,18 +54,59 @@ let UIController = (() => {
 
     let DOMstrings = {
         inputType: '.add__type',
-        inputDesc: '.add__description',
-        inputVal: '.add__value',
-        inputBtn: '.add__btn'
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputButton: '.add__btn',
+        incomeContainer: '.income__list',
+        expensesContainer: '.expenses__list'
+    }
+
+    let createElementFromHTML = (htmlString) => {
+        let div = document.createElement('div');
+        div.innerHTML = htmlString.trim();
+        return div.firstChild;
     }
 
     return {
         getInput: () => {
             return {
                 type: document.querySelector(DOMstrings.inputType).value,
-                desc: document.querySelector(DOMstrings.inputDesc).value,
-                val: document.querySelector(DOMstrings.inputVal).value
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value
             };
+        },
+
+        addItem: (obj, type) => {
+            let html, element;
+
+            // Create HTML and fill values
+            if (type === 'inc') {
+                element = DOMstrings.incomeContainer;
+                html = `<div class="item clearfix" id="income-${obj.id}">
+                    <div class="item__description">${obj.description}</div>
+                    <div class="right clearfix">
+                        <div class="item__value">${obj.value}</div>
+                        <div class="item__delete">
+                            <button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button>
+                        </div>
+                    </div>
+                </div>`;
+            } else if (type === 'exp') {
+                element = DOMstrings.expensesContainer;
+                html = `<div class="item clearfix" id="expense-${obj.id}">
+                    <div class="item__description">${obj.description}</div>
+                    <div class="right clearfix">
+                        <div class="item__value">${obj.value}</div>
+                        <div class="item__percentage">21%</div>
+                        <div class="item__delete">
+                            <button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button>
+                        </div>
+                    </div>
+                </div>`;
+            }
+
+            // Insert created HTML into the DOM
+            document.querySelector(element).insertAdjacentElement('beforeend', createElementFromHTML(html));
         },
 
         getDOMstring: () => {
@@ -33,16 +118,32 @@ let UIController = (() => {
 
 let appController = ((budgetCtrl, UICtrl) => {
 
-    let DOMstring = UICtrl.getDOMstring();
+    let setupListeners = () => {
+
+        let DOMstring = UICtrl.getDOMstring();
+
+        document.querySelector(DOMstring.inputButton).addEventListener('click', ctrlAddItem);
+
+        document.addEventListener('keypress', (event) => {
+
+            if (event.keyCode === 13 || event.which === 13) {
+                ctrlAddItem();
+            }
+        });
+    };
 
     let ctrlAddItem = () => {
+        let input, newItem;
 
         // 1. Get the field input data
-        let input = UICtrl.getInput();
+        input = UICtrl.getInput();
+        console.log('Input added: ' + JSON.stringify(input));
 
         // 2. Add the item to the budget controller
+        newItem = budgetCtrl.addItem(input.type, input.description, input.value);
 
         // 3. Add the item to the UI
+        UIController.addItem(newItem, input.type);
 
         // 4. Clear the fields
 
@@ -52,18 +153,13 @@ let appController = ((budgetCtrl, UICtrl) => {
 
     }
 
-    document.querySelector(DOMstring.inputBtn).addEventListener('click', ctrlAddItem);
-
-    document.addEventListener('keypress', (e) => {
-
-        if (e.keyCode === 13 || e.which === 13) {
-            ctrlAddItem();
-        }
-
-    });
-
     return {
-
+        init: () => {
+            console.log('Application has been started');
+            setupListeners();
+        }
     }
 
 })(budgetController, UIController);
+
+appController.init();
