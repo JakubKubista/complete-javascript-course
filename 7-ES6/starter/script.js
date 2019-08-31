93df673d49a93d84e62648b2133b249b4fdefb59
JakubKubista <xkubista@gmail.com>
2019-09-01 01:06:19 +0200

    Add ES6 Repetition - Solution of assignment

--- 7-ES6/starter/script.js
+++ 7-ES6/starter/script.js
@@ -507,3 +507,123 @@
 // john6.calculateAge();
 // john6.addMedal();
 // console.log(john6);
+
+
+
+/////////////////////////////////
+// CODING CHALLENGE
+
+/*
+
+Suppose that you're working in a small town administration, and you're in charge of two town elements:
+1. Parks
+2. Streets
+
+It's a very small town, so right now there are only 3 parks and 4 streets. All parks and streets have a name and a build year.
+
+At an end-of-year meeting, your boss wants a final report with the following:
+1. Tree density of each park in the town (forumla: number of trees/park area)
+2. Average age of each town's park (forumla: sum of all ages/number of parks)
+3. The name of the park that has more than 1000 trees
+4. Total and average length of the town's streets
+5. Size classification of all streets: tiny/small/normal/big/huge. If the size is unknown, the default is normal
+
+All the report data should be printed to the console.
+
+HINT: Use some of the ES6 features: classes, subclasses, template strings, default parameters, maps, arrow functions, destructuring, etc.
+
+*/
+
+class Element {
+  constructor(name, buildYear) {
+    this.name = name;
+    this.buildYear = buildYear;
+  }
+
+}
+
+class Park extends Element {
+  constructor(name, buildYear, area, countTrees) {
+    super (name, buildYear);
+    this.area = area;
+    this.countTrees = countTrees;
+  }
+
+  treeDensity() {
+    return (this.countTrees / this.area).toFixed(2);
+  }
+}
+
+class Street extends Element {
+  constructor(name, buildYear, length, size) {
+    super (name, buildYear);
+    this.length = length;
+    this.size = size;
+  }
+    
+  classifyStreet () {
+      const classification = new Map();
+      classification.set(1, 'tiny');
+      classification.set(2, 'small');
+      classification.set(3, 'normal');
+      classification.set(4, 'big');
+      classification.set(5, 'huge');
+      console.log(`${this.name}, build in ${this.buildYear}, is a ${classification.get(this.size)} street.`);
+  }
+}
+
+const arches = new Park ('Arches', 1356, 43, 529);
+const yellow = new Park ('Yellow Stone', 955, 12, 5025);
+const krkonose = new Park ('Krkonose', 356, 433, 1029);
+const parks = [arches, yellow, krkonose];
+
+const task1 = '1. Tree density of each park in the town: ';
+parks.forEach(element => console.log(task1 + element.treeDensity()));
+
+function getAvgParkAge() {
+  let sum = 0;
+  let ages = [...arguments][0];
+  ages.forEach(element => sum += parseInt(element.buildYear));
+  return (sum / ages.length).toFixed(2);
+}
+
+const task2 = '2. Average age of each towns park: ';
+const avgParkAge = getAvgParkAge(parks);
+console.log(task2 + avgParkAge);
+
+function hugePark() {
+  let _;
+  parks.forEach(element => {
+    _ = element.countTrees > 1000 ? element.name : 'Not found';
+  });
+  return _;
+}
+
+// Aternatively
+// const ages = parks.map(el => new Date().getFullYear() - el.buildYear);
+// const [totalAge, avgAge] = calc(ages);
+// console.log(`Our ${parks.length} parks have an average of ${avgAge} years.`);
+
+
+const task3 = '3. The name of the park that has more than 1000 trees: ';
+console.log(task3 + hugePark());
+
+// Aternatively
+// const i = parks.map(el => el.numTrees).findIndex(el => el >= 1000);
+// console.log(`${parks[i].name} has more than 1000 trees.`);
+
+const streets = [new Street('Ocean Avenue', 1999, 1.1, 4),
+                   new Street('Evergreen Street', 2008, 2.7, 2),
+                   new Street('4th Street', 2015, 0.8),
+                   new Street('Sunset Boulevard', 1982, 2.5, 5)];
+
+const [...lengthStreets] = streets.map((element) => element.length);
+const sumStreets = lengthStreets.reduce((a, b) => a + b);
+const avgStreets = (sumStreets / lengthStreets.length).toFixed(2);
+const task4 = '4. Total and average length of the towns streets: ';
+console.log(task4 + avgStreets);
+
+
+const task5 = '5. Size classification of all streets: tiny/small/normal/big/huge. If the size is unknown, the default is normal:';
+console.log(task5);
+streets.forEach(el => el.classifyStreet());
