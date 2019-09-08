1d185790573407af9953b23e650c7b94f68853b9
JakubKubista <xkubista@gmail.com>
2019-09-08 22:11:33 +0200

    Add list view

--- 9-forkify/starter/src/js/utils/base.js
+++ 9-forkify/starter/src/js/utils/base.js
@@ -6,5 +6,6 @@ export default {
   searchRes: document.querySelector(`.${constants.results}`),
   searchResList: document.querySelector(`.${constants.resultsList}`),
   searchResPages: document.querySelector(`.${constants.resultsPages}`),
-  recipeRes: document.querySelector(`.${constants.recipe}`)
+  recipeRes: document.querySelector(`.${constants.recipe}`),
+  shopping: document.querySelector(`.${constants.shopping}`),
 };
--- 9-forkify/starter/src/js/utils/constants.js
+++ 9-forkify/starter/src/js/utils/constants.js
@@ -5,5 +5,6 @@ export default {
   resultsList: 'results__list',
   resultsPages: 'results__pages',
   recipe: 'recipe',
+  shopping: 'shopping__list',
   loader: 'loader'
 }
